da0907146373e10b59189fd623d4710a72a77504
ylcnky <mehmet.yalcinkaya@aalto.fi>
2017-08-26 14:12:53 +0300

    notify disconnect js

---
+++ src/notifyDisconnect/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+AWS.config.region = process.env.IOT_AWS_REGION;
+const iotData = new AWS.iotData(
+  {
+  endpoint: process.env.IOT_ENDPOINT_HOST
+})
+
+exports.handler = (message) => {
+  let params = {
+    topic: 'client-disconnected',
+    payload : JSON.stringify(message),
+    qos: 0
+  };
+
+  iotData.publish(params, function(err, data){
+    if(err){
+      console.log('Unable to notify IoT of stories update: ${err}');
+    } else {
+      console.log('Successfully notified IoT of stories update');
+    }
+  });
+}
