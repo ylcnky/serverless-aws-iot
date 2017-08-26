9710951c9b1a546600846b7df3402e41f2302b49
ylcnky <mehmet.yalcinkaya@aalto.fi>
2017-08-26 14:24:11 +0300

    UserNamePrompt, ChatHeader and ChatWindow conf added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,4 +20,66 @@ class App extends Component {
   }
 }
 
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onSend = this.onSend.bind(this);
+        this.connect = this.connect.bind(this);
+
+        this.state = {
+            users: [],
+            messages: [],
+            clientId: getClientId(),
+            isConnected: false,
+        };
+    }
+
+    connect(username) {
+        this.setState({ username });
+
+        this.client = new RealtimeClient(this.state.clientId, username);
+
+        this.client.connect()
+            .then(() => {
+                this.setState({ isConnected: true });
+                this.client.onMessageReceived((topic, message) => {
+                    if (topic === "client-connected") {
+                        this.setState({ users: [...this.state.users, message] })
+                    } else if (topic === "client-disconnected") {
+                        this.setState({ users: this.state.users.filter(user => user.clientId !== message.clientId) })
+                    } else {
+                        this.setState({ messages: [...this.state.messages, message] });
+                    }
+                })
+            })
+    }
+
+    onSend(message) {
+        this.client.sendMessage({
+            username: this.state.username,
+            message: message,
+            id: getMessageId(),
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <ChatHeader
+                    isConnected={ this.state.isConnected }
+                />
+                <ChatWindow
+                    users={ this.state.users }
+                    messages={ this.state.messages }
+                    onSend={ this.onSend }
+                />
+                <UserNamePrompt
+                    onPickUsername={ this.connect }
+                />
+            </div>
+        );
+    }
+}
+
 export default App;
