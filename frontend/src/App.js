3c929af02c37585cebd7cc19bfe7bd39fde84255
ylcnky <mehmet.yalcinkaya@aalto.fi>
2017-08-26 14:21:02 +0300

    RealTimeCliend configured

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 class App extends Component {
   render() {
